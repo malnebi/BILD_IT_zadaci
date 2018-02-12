11dc3a2cf2a019caa3f4360df725bc5b2c6bf954
Maja Vasilic <rmaja7@gmail.com>
2018-02-12 14:26:22 +0100

    firs commit

---
+++ src/Z_12_2_2018/MaxValue.java
@@ -0,0 +1,33 @@
+package Z_12_2_2018;
+
+public class MaxValue {
+	/**
+	 * 1. Napisati sljedeæu metodu koja vraæa najveæu vrijednosti u ArrayListu
+	 * Integera. Metoda vraæa null ukoliko je lista null iil ukoliko lista
+	 * sadrži 0 elemenata. public static Integer max(ArrayList<Integer> list)
+	 * 
+	 * 2. Pretpostavimo da su slova A, E, I, O i U samoglasnici. (Smjela
+	 * pretpostavka, znam) Napisati program koji pita korisnika da unese string
+	 * te mu ispište broj samoglasnika i suglasnika u datom stringu.
+	 * 
+	 * 3. Napisati metodu koja prima dva cijela broja kao argumente te vraæa
+	 * najveæi zajednièki djelilac za ta dva broja.
+	 * 
+	 * PRIMJER:
+	 * 
+	 * Unesite prvi broj: 125 Unesite drugi broj: 2525 Najveæi zajednièki
+	 * djelilac za brojeve 125 i 2525 je 25.
+	 * 
+	 * 4. Napišite program koji generiše 100 nasumiènih cijelih brojeva izmeðu 0
+	 * i 9 te ispisuje koliko se puta koji broj ponovio.
+	 * 
+	 * 5. Napisati program koji uèitava niz brojeva, pronalazi najveæi broj te
+	 * ispisuje koliko se puta taj broj ponavlja. Pretpostavimo da se unos
+	 * brojeva završava sa nulom.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
