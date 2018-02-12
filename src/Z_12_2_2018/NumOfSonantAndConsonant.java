c53fac38e60f78a8a0d4a4c960d8da504609f6fc
Maja Vasilic <rmaja7@gmail.com>
2018-02-12 18:59:02 +0100

    gitignore

---
+++ src/Z_12_2_2018/NumOfSonantAndConsonant.java
@@ -0,0 +1,14 @@
+package Z_12_2_2018;
+
+public class NumOfSonantAndConsonant {
+	/**
+	 * 2. Pretpostavimo da su slova A, E, I, O i U samoglasnici. (Smjela
+	 * pretpostavka, znam) Napisati program koji pita korisnika da unese string
+	 * te mu ispište broj samoglasnika i suglasnika u datom stringu.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
