36904cdf8cb1422764a1566cb94f08ece2193102
Maja Vasilic <rmaja7@gmail.com>
2018-02-15 00:11:45 +0100

    postavka_metoda

---
+++ src/zadaci_14_2_2018/Z1_.java
@@ -0,0 +1,10 @@
+package zadaci_14_2_2018;
+
+public class Z1_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/zadaci_14_2_2018/Z5_.java
@@ -0,0 +1,15 @@
+package zadaci_14_2_2018;
+
+public class Z5_ {
+	/**
+	 * 5. Napisati metodu koja broji slova u stringu. Metoda treba koristiti
+	 * sljedeæi header: public static int countLetters(String s). Napisati
+	 * program koji pita korisnika da unese neki string te mu vrati koliko taj
+	 * string ima slova.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
